a4eb17642b7268ff9215297490ba687ae89b9da2
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-06-29 23:59:31 +0530

    Third commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,32 @@
 const router = require('express').Router();
-
-const authController = require('../controllers/auth');
-
-router.post('/register', authController().register);
+const User = require('../model/User');
+const {registerValidation, loginValidation} = require('../validation');
 
 
+router.post('/register', async (req, res)=> {
+    
+    //VALIDATING THE DATA
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    //Checking if user in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+    
+    
+    //Create a new User
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
+});
 
 
 module.exports = router;
