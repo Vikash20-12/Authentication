839e591f8ce97d73e09b937201c9841cd1860eae
Vikash20-12 <85476256+Vikash20-12@users.noreply.github.com>
2021-06-30 00:14:15 +0530

    fourth commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation');
 
 
@@ -13,16 +14,21 @@ router.post('/register', async (req, res)=> {
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
     
-    
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+
+
     //Create a new User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id});
     }catch(err){
         res.status(400).send(err);
     }
